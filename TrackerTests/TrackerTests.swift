c7ba17d4b0934bcf64cf411ddd88543c4edfacd6
LightKrite <egorpartenko@icloud.com>
2024-07-09 20:35:58 +0300

    tests

--- TrackerTests/TrackerTests.swift
+++ TrackerTests/TrackerTests.swift
@@ -1,5 +1,20 @@
 import XCTest
 import SnapshotTesting
+@testable import Tracker
 
-
+final class TrackerTests: XCTestCase {
+    
+    func testMainViewController() {
+        let vc = TrackersListViewController()
+        
+        assertSnapshots(matching: vc, as: [.image(traits: UITraitCollection(userInterfaceStyle: .light))])
+    }
+    
+    func testMainViewControllerDark() {
+        let vc = TrackersListViewController()
+        
+        assertSnapshots(matching: vc, as: [.image(traits: UITraitCollection(userInterfaceStyle: .dark))])
+    }
+    
+}
 
